5c4213972874faa71deceb1551e0312e1173d12e
pbentes <pedrogomes2000.3@gmail.com>
2024-02-12 00:19:10 +0000

    Removed comments

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -37,10 +37,7 @@ __declspec(dllexport) int engine_main() {
 	if (!window)
 		return 1;
 	glfwSetKeyCallback(window, glfw_keyCallback);
-	// Call bgfx::renderFrame before bgfx::init to signal to bgfx not to create a render thread.
-	// Most graphics APIs must be used on the same thread that created the window.
 	bgfx::renderFrame();
-	// Initialize bgfx using the native window handle and window resolution.
 
 	bgfx::Init init;
     
@@ -60,25 +57,25 @@ __declspec(dllexport) int engine_main() {
 	init.resolution.reset = BGFX_RESET_VSYNC;
 	if (!bgfx::init(init))
 		return 1;
-	// Set view 0 to the same dimensions as the window and to clear the color buffer.
-	const bgfx::ViewId kClearView = 0;
+
+    const bgfx::ViewId kClearView = 0;
 	bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
 	bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
-		// Handle window resize.
-		int oldWidth = width, oldHeight = height;
+
+        int oldWidth = width, oldHeight = height;
 		glfwGetWindowSize(window, &width, &height);
 		if (width != oldWidth || height != oldHeight) {
 			bgfx::reset((uint32_t)width, (uint32_t)height, BGFX_RESET_VSYNC);
 			bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
 		}
-		// This dummy draw call is here to make sure that view 0 is cleared if no other draw calls are submitted to view 0.
-		bgfx::touch(kClearView);
+
+        bgfx::touch(kClearView);
 
 		bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
-		// Advance to next frame. Process submitted rendering primitives.
-		bgfx::frame();
+
+        bgfx::frame();
 	}
 	bgfx::shutdown();
 	glfwTerminate();
