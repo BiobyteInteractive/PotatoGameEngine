8b3769d90e23d90697ae6e64dd2fb883a1706cbb
pbentes <pedrogomes2000.3@gmail.com>
2024-02-12 01:12:57 +0000

    - Added nuklear for use in the games; - Added imgui for use in the editor;

--- setup.ps1
+++ setup.ps1
@@ -8,4 +8,13 @@ $Env:VCPKG_DEFAULT_TRIPLET = "x64-windows"
 .\vendor\vcpkg\vcpkg.exe install entt;
 .\vendor\vcpkg\vcpkg.exe install lua;
 
+# Downloads the latest version of nuklear straight from their github. (The VCPKG version is outdated)
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Immediate-Mode-UI/Nuklear/master/nuklear.h" -OutFile (Join-Path ((Get-Location).Path) "vendor\vcpkg\installed\x64-windows\include\nuklear.h") 
+
+# Imgui for the editor
+.\vendor\vcpkg\vcpkg.exe install imgui;
+.\vendor\vcpkg\vcpkg.exe install imgui[docking-experimental];
+.\vendor\vcpkg\vcpkg.exe install imgui[glfw-binding];
+.\vendor\vcpkg\vcpkg.exe install imgui-node-editor;
+
 .\vendor\windows\premake5\premake5.exe vs2022;
