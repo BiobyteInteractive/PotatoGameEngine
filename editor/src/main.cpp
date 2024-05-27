4bdc51b1e60ac63187d7bd953b70c8488679357d
pbentes <pedrogomes2000.3@gmail.com>
2024-05-27 18:18:32 +0100

    Tested the engine in the editor project

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <potato.h>
+
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello from the editor!" << std::endl;
+    
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, (char*)"potato");
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(255, 0, 255, 255);
+        EndDrawing();
+    }
+    
     return 0;
 }
