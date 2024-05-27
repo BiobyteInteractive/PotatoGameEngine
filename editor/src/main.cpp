751d7f492d726fa78667e5bd753ec8aca8e6944c
pbentes <pedrogomes2000.3@gmail.com>
2024-05-27 22:27:24 +0100

    First ImGui test

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,17 +1,41 @@
 #include <potato.h>
 
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 int main() {
-    
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, (char*)"potato");
+    InitWindow(screenWidth, screenHeight, (char*)"Potato Game Engine Editor");
+    GLFWwindow* window = GetWindow();
 
+    const char* glsl_version = "#version 330";
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    bool show_demo_window = true;
     while(!WindowShouldClose()) {
         BeginDrawing();
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
             ClearBackground(255, 0, 255, 255);
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         EndDrawing();
     }
     
