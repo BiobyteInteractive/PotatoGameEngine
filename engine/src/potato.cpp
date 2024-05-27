619571b619821b0df86a308ff68b59e90ece5d53
pbentes <pedrogomes2000.3@gmail.com>
2024-05-27 18:18:19 +0100

    Added some test functions to the engine project

---
+++ engine/src/potato.cpp
@@ -0,0 +1,74 @@
+#include "potato.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+GLFWwindow* window;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int InitWindow(int screenWidth, int screenHeight, char* windowTitle) 
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    window = glfwCreateWindow(screenWidth, screenHeight, windowTitle, NULL, NULL);
+    if(window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    return 0; 
+}
+
+bool WindowShouldClose() {
+    return glfwWindowShouldClose(window);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void BeginDrawing() {
+    processInput(window);
+}
+
+void EndDrawing() {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+void ClearBackground(int r, int g, int b, int a) {
+    glClearColor((float)r/255, (float)g/255, (float)b/255, (float)a/255);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
--- engine/src/potato.h
+++ engine/src/potato.h
@@ -2,4 +2,12 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+int InitWindow(int screenWidth, int screenHeight, char* windowTitle);
+bool WindowShouldClose();
+
+void BeginDrawing();
+void EndDrawing();
+
+void ClearBackground(int r, int g, int b, int a);
+
 #endif
