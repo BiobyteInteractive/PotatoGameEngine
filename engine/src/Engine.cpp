cc9419d4d9bd4955b305c3367ca6d2603c6e292c
pbentes <pedrogomes2000.3@gmail.com>
2024-05-29 20:18:38 +0100

    Renamed engine

--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "potato.h"
+#include "engine.h"
 
 #include <iostream>
 
@@ -44,16 +44,12 @@ bool WindowShouldClose() {
     return glfwWindowShouldClose(window);
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    //if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    //    glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
