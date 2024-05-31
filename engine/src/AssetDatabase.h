28d44ae38e4b77dac509120ce06139f53fb4ccf8
pbentes <pedrogomes2000.3@gmail.com>
2024-05-31 13:22:42 +0100

    Its all one class now

--- engine/src/AssetDatabase.h
+++ engine/src/AssetDatabase.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #ifndef ASSET_MANAGER_H
 #define ASSET_MANAGER_H
 
@@ -16,24 +17,26 @@ typedef struct Asset {
 
 } Asset;
 
-class FileUpdateListener : public efsw::FileWatchListener {
-    public:
-        void handleFileAction(efsw::WatchID watchid, const std::string& dir, const std::string& filename, efsw::Action action, std::string oldFilename) override;
-};
-
-class DllExport AssetDatabase {
+class DllExport AssetDatabase : public efsw::FileWatchListener {
     public:
         AssetDatabase(std::string directory);
         ~AssetDatabase();
+
+        void handleFileAction(efsw::WatchID watchid, const std::string& dir, const std::string& filename, efsw::Action action, std::string oldFilename) override;
 
         std::unique_ptr<Asset> GetAssetById(int id);
         std::unique_ptr<Asset> GetAssetByPath(std::string path);
         std::unique_ptr<std::vector<Asset>> GetAssetsByDirectory(std::string directory);
         std::unique_ptr<std::vector<Asset>> GetAssetsByExtension(std::string extension);
+
+    private:
+        void InsertAsset(std::string directory, std::string filename);
+        void UpdateAsset(std::string directory, std::string filename, std::string old_filename);
+        void DeleteAsset(std::string directory, std::string filename);
+        void Cleanup();
+        
     private:
         sqlite3* asset_db;
-
-        FileUpdateListener* file_update_listener;
         efsw::FileWatcher* file_watcher;
         efsw::WatchID watch_id;
 };
