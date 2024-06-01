27de462f2beb80a143693a2ab259bf4d642960d2
pbentes <pedrogomes2000.3@gmail.com>
2024-06-01 22:58:47 +0100

    Created the initial version Asset class

---
+++ engine/src/Asset.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstddef>
+#include <string>
+#ifndef ASSET_H
+#define ASSET_H
+
+#define DllExport __declspec( dllexport )
+
+class DllExport Asset {
+    public:
+        Asset();
+        ~Asset();
+
+        void LoadAsset();
+        void UnloadAsset();
+    private:
+        void ImportAsset();
+    public:
+        size_t m_Id;
+        std::string m_Path;
+        std::string m_Directory;
+        std::string m_Extension;
+        bool m_Deleted;
+};
+
+#endif
