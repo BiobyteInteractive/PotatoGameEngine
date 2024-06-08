483eebbee2d10fc2e4751c971c6864c0a3ee014a
pbentes <pedrogomes2000.3@gmail.com>
2024-06-08 19:25:35 +0100

    Asset Importer is an Interface

--- engine/src/Assets/AssetImporter.h
+++ engine/src/Assets/AssetImporter.h
@@ -1,18 +1,18 @@
 #pragma once
-#include <filesystem>
 #ifndef ASSET_IMPORTER_H
 #define ASSET_IMPORTER_H
 
 #define DllExport __declspec( dllexport )
 
+#include <filesystem>
 #include <memory>
 #include <vector>
 
 class DllExport AssetImporter {
     public:
-        AssetImporter();
-        ~AssetImporter();
-        std::shared_ptr<std::vector<unsigned char>> ImportAsset(std::filesystem::path assetPath);
+        AssetImporter() = default;
+        ~AssetImporter() = default;
+        virtual std::shared_ptr<std::vector<unsigned char>> ImportAsset(std::filesystem::path assetPath) = 0;
 };
 
 #endif
