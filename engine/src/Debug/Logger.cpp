ed3cc5202ad29360a14deb087d3913f068e020a2
pbentes <pedrogomes2000.3@gmail.com>
2024-06-09 15:33:49 +0100

    Added a logger

--- engine/src/Debug/Logger.cpp
+++ engine/src/Debug/Logger.cpp
@@ -1,21 +1,36 @@
 #include "Logger.h"
 
-Logger::Logger() {
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <string>
 
+Logger::Logger() {
+    this->m_OutputStream.open("engine.log");
 }
 
 Logger::~Logger() {
-
+    this->m_OutputStream.close();
 }
 
 void Logger::Error(const std::string message) {
-
+    this->Log(message, "ERROR"); 
 }
 
 void Logger::Debug(const std::string message) {
-
+    this->Log(message, "DEBUG"); 
 }
 
 void Logger::Info(const std::string message) {
+    this->Log(message, "INFO"); 
+}
 
+void Logger::Log(const std::string message, const std::string mode) {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+    struct tm now_tm;
+    localtime_s(&now_tm, &now_time_t);
+
+    this->m_OutputStream << "[" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "] [" << mode << "] " << message << std::endl;
+    this->m_OutputStream.flush(); 
 }
--- engine/src/Debug/Logger.h
+++ engine/src/Debug/Logger.h
@@ -4,16 +4,29 @@
 
 #define DllExport __declspec( dllexport )
 
+#include <fstream>
 #include <string>
 
 class DllExport Logger {
     public:
-        Logger();
-        ~Logger();
+        static Logger& GetInstance() {
+            static Logger instance;
+            return instance;
+        }
+
+        Logger(const Logger&) = delete;
+        Logger& operator=(const Logger&) = delete;
 
         void Error(const std::string message);
         void Debug(const std::string message);
         void Info(const std::string message);
+    private:
+
+        Logger();
+        ~Logger();
+        void Log(const std::string message, const std::string mode);
+    public:
+        std::ofstream m_OutputStream;
 };
 
 #endif
