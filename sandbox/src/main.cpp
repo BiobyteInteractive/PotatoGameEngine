97f29a9a5058329ad77c205be4ae8a8916162f4f
pbentes <pedrogomes2000.3@gmail.com>
2024-06-10 19:44:36 +0100

    Setup the renderer in the sandbox

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include "GLFW/glfw3.h"
+#include "Renderer/Renderer.h"
+#include <Core/Application.h>
 
 int main() {
-    std::cout << "Hello from the sandbox!" << std::endl;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    Application* app = new Application(screenWidth, screenHeight, "Sandbox");
+    GLFWwindow* window = app->GetWindow();
+    Renderer* renderer = app->m_Renderer;
+
+    while(!app->WindowShouldClose()) {
+        renderer->BeginDrawing(window);
+
+        renderer->ClearBackground(255, 0, 255, 255);
+
+        renderer->EndDrawing(window);
+    }
+
     return 0;
 }
