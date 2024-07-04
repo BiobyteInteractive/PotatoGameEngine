b9a8b8dcb5dc8964eba6d509332988cd5a8c62aa
pbentes <pedrogomes2000.3@gmail.com>
2024-07-04 19:11:58 +0100

    Added a Shader class

---
+++ engine/src/Renderer/Shader.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define DllExport __declspec( dllexport )
+
+#include <string>
+
+namespace Engine {
+    class DllExport Shader {
+        public:
+            Shader(const char* vertexPath, const char* fragmentPath);
+            
+            void Use() const;
+            void SetBool(const std::string& name, bool value) const;
+            void SetInt(const std::string& name, int value) const;
+            void SetFloat(const std::string& name, float value) const;
+        public:
+            unsigned int m_Id;
+
+        private:
+            void CheckCompileErrors(unsigned int shader, std::string type);
+    };
+}
