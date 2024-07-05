bd33a8f3bc5a35edb7896a2a963933fb5fa08877
pbentes <pedrogomes2000.3@gmail.com>
2024-07-05 18:03:43 +0100

    Added an implementation for the color class

---
+++ engine/src/Renderer/Color.cpp
@@ -0,0 +1,104 @@
+#include "Color.h"
+
+#include <format>
+#include <tuple>
+
+namespace Engine {
+    Color::Color(int r, int g, int b, int a) {
+        this->r = r / 255.0f;
+        this->g = g / 255.0f;
+        this->b = b / 255.0f;
+        this->a = a / 255.0f;
+    }
+    
+    Color::Color(float r, float g, float b, float a) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+        this->a = a;
+    }
+
+    std::string Color::ToString() {
+        return std::format("({}, {}, {}, {})", this->r, this->g, this->b, this->a);
+    }
+
+    Color Color::HSVToRGB(float h, float s, float v) {
+        float c = v * s;
+        float x = c * (1 - std::fabs(fmod(h / 60.0, 2) - 1));
+        float m = v - c;
+
+        float r, g, b;
+        if (0 <= h && h < 60) {
+            r = c; g = x; b = 0;
+        } else if (60 <= h && h < 120) {
+            r = x; g = c; b = 0;
+        } else if (120 <= h && h < 180) {
+            r = 0; g = c; b = x;
+        } else if (180 <= h && h < 240) {
+            r = 0; g = x; b = c;
+        } else if (240 <= h && h < 300) {
+            r = x; g = 0; b = c;
+        } else if (300 <= h && h < 360) {
+            r = c; g = 0; b = x;
+        } else {
+            r = 0; g = 0; b = 0; // Hue out of range
+        }
+
+        int R = static_cast<int>((r + m) * 255);
+        int G = static_cast<int>((g + m) * 255);
+        int B = static_cast<int>((b + m) * 255);
+
+        Color color(R, G, B, 255);
+        return color;
+    }
+
+    std::tuple<float, float, float> Color::RGBToHSV(Color rgbColor) {
+        float r = rgbColor.r / 255.0f;
+        float g = rgbColor.g / 255.0f;
+        float b = rgbColor.b / 255.0f;
+
+        float max = std::max({r, g, b});
+        float min = std::min({r, g, b});
+        float delta = max - min;
+
+        float h = 0, s = 0, v = 0;
+
+        v = max; // Value
+
+        if (max == 0.0f) {
+            s = 0.0f; // Saturation
+        } else {
+            s = delta / max; // Saturation
+        }
+
+        if (delta == 0.0f) {
+            h = 0.0f; // Hue
+        } else {
+            if (max == r) {
+                h = 60.0f * (fmod(((g - b) / delta), 6.0f));
+            } else if (max == g) {
+                h = 60.0f * (((b - r) / delta) + 2.0f);
+            } else if (max == b) {
+                h = 60.0f * (((r - g) / delta) + 4.0f);
+            }
+
+            if (h < 0.0f) {
+                h += 360.0f;
+            }
+        }
+
+        return std::make_tuple(h, s, v);
+    }
+
+    Color Color::black = Color(0,0,0,255);
+    Color Color::blue = Color(0, 0, 255, 255);
+    Color Color::clear = Color(0, 0, 0, 0);
+    Color Color::cyan = Color(0, 255, 255, 255);
+    Color Color::gray = Color(127, 127, 127, 255);
+    Color Color::green = Color(0, 255, 0, 255);
+    Color Color::grey = Color(127, 127, 127, 255);
+    Color Color::magenta = Color(255, 0, 255, 0);
+    Color Color::red = Color(255, 0, 0, 255);
+    Color Color::white = Color(255, 255, 255, 255);
+    Color Color::yellow = Color(255, 235, 4, 255);
+}
--- engine/src/Renderer/Color.h
+++ engine/src/Renderer/Color.h
@@ -3,9 +3,10 @@
 #define DllExport __declspec( dllexport )
 
 #include <string>
+#include <tuple>
 
 namespace Engine {
-    class Color {
+    class DllExport Color {
         public:
             Color(int r, int g, int b, int a);
             Color(float r, float g, float b, float a);
@@ -13,22 +14,22 @@ namespace Engine {
             std::string ToString();
 
             static Color HSVToRGB(float h, float s, float v);
-            static void  RGBToHSV(Color rgbColor, float* h, float* s, float* v);
-            static Color Lerp(Color from, Color to, float t);
-            static Color LerpUnclamped(Color from, Color to, float t);
+            static std::tuple<float, float, float>  RGBToHSV(Color rgbColor);
+            //static Color Lerp(Color from, Color to, float t);
+            //static Color LerpUnclamped(Color from, Color to, float t);
         public:
             float r, g, b, a;
 
-            static Color black;
-            static Color blue;
-            static Color clear;
-            static Color cyan;
-            static Color gray;
-            static Color green;
-            static Color grey;
-            static Color magenta;
-            static Color red;
-            static Color white;
-            static Color yellow;
+            static Color black;   // These don't work??? Unresolved symbols.
+            static Color blue;    // These don't work??? Unresolved symbols.
+            static Color clear;   // These don't work??? Unresolved symbols.
+            static Color cyan;    // These don't work??? Unresolved symbols.
+            static Color gray;    // These don't work??? Unresolved symbols.
+            static Color green;   // These don't work??? Unresolved symbols.
+            static Color grey;    // These don't work??? Unresolved symbols.
+            static Color magenta; // These don't work??? Unresolved symbols.
+            static Color red;     // These don't work??? Unresolved symbols.
+            static Color white;   // These don't work??? Unresolved symbols.
+            static Color yellow;  // These don't work??? Unresolved symbols.
     };
 }
