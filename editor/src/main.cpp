44c6c2fc3327cd5fbde2557d15f6e07415480a5c
pbentes <pedrogomes2000.3@gmail.com>
2024-07-15 14:00:21 +0100

    Added an open file dialog to open the project

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -26,6 +26,7 @@
 #ifdef _WIN32
     #include <Windows.h>
     #include <winbase.h>
+    #include <shobjidl.h> 
 #endif
 
 using namespace Engine;
@@ -42,12 +43,67 @@ int main(int argc, char* argv[]) {
         SetDllDirectory(std::filesystem::current_path().string().c_str());
     #endif
 
+    std::string projectPath;
     
     if (argc <= 1 || !std::filesystem::is_regular_file(argv[1])) {
         Logger::GetInstance().Error("No path to the project provided. Aborting.");
-        abort();
+        HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | 
+        COINIT_DISABLE_OLE1DDE);
+        if (SUCCEEDED(hr))
+        {
+            IFileOpenDialog *pFileOpen;
+
+            // Create the FileOpenDialog object.
+            hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, 
+                    IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+            if (SUCCEEDED(hr))
+            {
+                // Show the Open dialog box.
+                hr = pFileOpen->Show(NULL);
+
+                // Get the file name from the dialog box.
+                if (SUCCEEDED(hr))
+                {
+                    IShellItem *pItem;
+                    hr = pFileOpen->GetResult(&pItem);
+                    if (SUCCEEDED(hr))
+                    {
+                        PWSTR pszFilePath;
+                        hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+                        // Display the file name to the user.
+                        if (SUCCEEDED(hr))
+                        {
+                            if (pszFilePath == nullptr) {
+                                projectPath = "";
+                            }
+
+                            // Get the length of the resulting string
+                            int size_needed = WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, nullptr, 0, nullptr, nullptr);
+
+                            // Allocate a string with the needed size
+                            std::string strTo(size_needed, 0);
+
+                            // Perform the conversion
+                            WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, &strTo[0], size_needed, nullptr, nullptr);
+
+                            // Remove the null terminator that WideCharToMultiByte adds
+                            strTo.resize(size_needed - 1);
+
+                            projectPath = strTo;
+                        }
+                        pItem->Release();
+                    }
+                }
+                pFileOpen->Release();
+            }
+            CoUninitialize();
+        }
+    } else {
+        projectPath = std::string(argv[1]);
     }
-    std::filesystem::current_path(std::filesystem::path(argv[1]).remove_filename().string());
+    std::filesystem::current_path(std::filesystem::path(projectPath).remove_filename().string());
 
     const int screenWidth = 800;
     const int screenHeight = 450;
