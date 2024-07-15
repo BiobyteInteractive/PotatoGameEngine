6139c615aba56bca3eb92b085f9086999c25a9ee
pbentes <pedrogomes2000.3@gmail.com>
2024-07-15 19:02:34 +0100

    Retry the file selection dialog

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -47,58 +47,60 @@ int main(int argc, char* argv[]) {
     
     if (argc <= 1 || !std::filesystem::is_regular_file(argv[1])) {
         Logger::GetInstance().Info("No path to the project provided. Opening file prompt.");
-        HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | 
-        COINIT_DISABLE_OLE1DDE);
-        if (SUCCEEDED(hr))
-        {
-            IFileOpenDialog *pFileOpen;
-
-            // Create the FileOpenDialog object.
-            hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, 
-                    IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
-
+        while(projectPath.empty() || !std::filesystem::path(projectPath).has_filename()) {
+            HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | 
+            COINIT_DISABLE_OLE1DDE);
             if (SUCCEEDED(hr))
             {
-                // Show the Open dialog box.
-                hr = pFileOpen->Show(NULL);
+                IFileOpenDialog *pFileOpen;
 
-                // Get the file name from the dialog box.
+                // Create the FileOpenDialog object.
+                hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, 
+                        IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
                 if (SUCCEEDED(hr))
                 {
-                    IShellItem *pItem;
-                    hr = pFileOpen->GetResult(&pItem);
+                    // Show the Open dialog box.
+                    hr = pFileOpen->Show(NULL);
+
+                    // Get the file name from the dialog box.
                     if (SUCCEEDED(hr))
                     {
-                        PWSTR pszFilePath;
-                        hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-
-                        // Display the file name to the user.
+                        IShellItem *pItem;
+                        hr = pFileOpen->GetResult(&pItem);
                         if (SUCCEEDED(hr))
                         {
-                            if (pszFilePath == nullptr) {
-                                projectPath = "";
+                            PWSTR pszFilePath;
+                            hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+                            // Display the file name to the user.
+                            if (SUCCEEDED(hr))
+                            {
+                                if (pszFilePath == nullptr) {
+                                    projectPath = "";
+                                }
+
+                                // Get the length of the resulting string
+                                int size_needed = WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, nullptr, 0, nullptr, nullptr);
+
+                                // Allocate a string with the needed size
+                                std::string strTo(size_needed, 0);
+
+                                // Perform the conversion
+                                WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, &strTo[0], size_needed, nullptr, nullptr);
+
+                                // Remove the null terminator that WideCharToMultiByte adds
+                                strTo.resize(size_needed - 1);
+
+                                projectPath = strTo;
                             }
-
-                            // Get the length of the resulting string
-                            int size_needed = WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, nullptr, 0, nullptr, nullptr);
-
-                            // Allocate a string with the needed size
-                            std::string strTo(size_needed, 0);
-
-                            // Perform the conversion
-                            WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, &strTo[0], size_needed, nullptr, nullptr);
-
-                            // Remove the null terminator that WideCharToMultiByte adds
-                            strTo.resize(size_needed - 1);
-
-                            projectPath = strTo;
+                            pItem->Release();
                         }
-                        pItem->Release();
                     }
+                    pFileOpen->Release();
                 }
-                pFileOpen->Release();
+                CoUninitialize();
             }
-            CoUninitialize();
         }
     } else {
         projectPath = std::string(argv[1]);
@@ -136,7 +138,7 @@ int main(int argc, char* argv[]) {
     Engine::ImGui::SetImGuiContext(::ImGui::GetCurrentContext());
     
     try {
-        Theme theme((std::filesystem::path(getExecutablePath()) / "Themes\\vs.toml").string());
+        Theme theme((std::filesystem::path(getExecutablePath()) / "Themes\\steam.toml").string());
         theme.SetTheme();
     } catch(...) {}
 
