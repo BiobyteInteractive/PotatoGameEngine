6f85b7a4b23d657ef3885cef537f7eeda5387cf0
pbentes <pedrogomes2000.3@gmail.com>
2024-07-28 01:34:05 +0100

    Started redoing the build system for the engine projects

---
+++ CopyDLL.cmake
@@ -0,0 +1,11 @@
+cmake_minimum_required(VERSION 3.16 FATAL_ERROR)
+
+file(GLOB_RECURSE DLL_FILES
+    "${BINARY_DIR}/_deps/*.dll"
+    "${BINARY_DIR}/engine/*.dll"
+    "${SOURCE_DIR}/vendor/*.dll"
+)
+
+foreach(DLL ${DLL_FILES})
+    file(COPY ${DLL} DESTINATION ${DLL_OUTPUT_DIR})
+endforeach()
