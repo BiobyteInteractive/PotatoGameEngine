ac71fdb1413307563602d8df30a74de8f73d28d9
pbentes <pedrogomes2000.3@gmail.com>
2024-08-24 00:38:06 +0100

    No longer need an application class

--- engine/src/Core/Application.cpp
+++
@@ -1,8 +0,0 @@
-#include "Application.h"
-#include "SceneManager.h"
-#include <filesystem>
-
-namespace Engine {
-    Application::Application(): m_AssetDatabase((std::filesystem::current_path() / "assets").string()), m_SceneManager() {
-    }
-}
