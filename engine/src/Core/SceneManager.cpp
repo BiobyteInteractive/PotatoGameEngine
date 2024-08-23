6fc43b511c9dc3b10ed7bcbe772fd7f29bc3b2c0
pbentes <pedrogomes2000.3@gmail.com>
2024-08-24 00:39:26 +0100

    I'm testing the scene manager

--- engine/src/Core/SceneManager.cpp
+++ engine/src/Core/SceneManager.cpp
@@ -1,13 +1,45 @@
 #include "SceneManager.h"
+#include "flecs/addons/cpp/world.hpp"
+#include "flecs/addons/flecs_c.h"
+#include "flecs/private/addons.h"
+#include <flecs/addons/json.h>
+#include <string>
+#include <iostream>
+#include <flecs/addons/meta.h>
+
 
 namespace Engine {
-    SceneManager::SceneManager() {
-        m_World = flecs::world();
+    #undef ECS_META_IMPL
+    #ifndef FLECS_COMPONENTS_TRANSFORM_IMPL
+    #define ECS_META_IMPL EXTERN // Ensure meta symbols are only defined once
+    #endif
 
-        m_World.set_threads(4);
+    ECS_STRUCT(Position, {
+        float x;
+        float y;
+    });
+
+    ECS_COMPONENT_DECLARE(Positon);
+
+    SceneManager::SceneManager(): m_World(flecs::world()), m_App(flecs::app_builder(m_World)){
+        const char* code = R"( using flecs.meta
+ 
+struct Position {
+  x = f32
+  y = f32
+}
+ 
+my_entity {
+  Position: {x: 10, y: 20}
+})";
+
+        m_World.script_run("position", code);
+
+        std::cout << m_World.to_json() << std::endl;
     }
+    SceneManager::SceneManager(SceneManager::AppSettings settings): m_World(flecs::world()), m_App(flecs::app_builder(m_World)){}
+    SceneManager::SceneManager(std::string settings): m_World(flecs::world()), m_App(flecs::app_builder(m_World)){}
 
-    void SceneManager::LoadScene() {}
-
-    void SceneManager::UnloadScene() {}
+    void SceneManager::LoadScene(std::string name, std::string scene) {}
+    void SceneManager::UnloadScene(std::string name) {}
 }
--- engine/src/Core/SceneManager.h
+++ engine/src/Core/SceneManager.h
@@ -5,17 +5,29 @@
 #endif
 
 #include <flecs.h>
+#include <flecs/addons/cpp/mixins/app/builder.hpp>
 #include <flecs/addons/cpp/world.hpp>
+#include <flecs/addons/meta.h>
+
+#include <string>
+
+
 
 namespace Engine {
+
     class SceneManager {
         public:
+            struct AppSettings {};
+        public:
             SceneManager();
+            SceneManager(AppSettings settings);
+            SceneManager(std::string settings);
             ~SceneManager() = default;
 
-            void LoadScene();
-            void UnloadScene();
+            void LoadScene(std::string name, std::string scene);
+            void UnloadScene(std::string name);
         public:
             flecs::world m_World;
+            flecs::app_builder m_App;
     };
 }
