0c017052f6c05856bcce09a5af37e7d5ea94cc77
pbentes <pedrogomes2000.3@gmail.com>
2024-08-24 00:38:30 +0100

    Added runtime project

---
+++ runtime/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <Core/SceneManager.h>
+
+int main() {
+    Engine::SceneManager* SceneManager = new Engine::SceneManager();
+    std::cout << "Hello runtime!" << std::endl;
+}
